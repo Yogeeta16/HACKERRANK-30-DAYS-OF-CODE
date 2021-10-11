966091c696214cc3428e73b220f8938eab2ad712
yogeeta bodade <90635845+Yogeeta16@users.noreply.github.com>
2021-10-11 13:42:07 +0530

    Create day_9.java

---
+++ day_9.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.close();
+        System.out.println(factorial(n));
+    }
+
+    private static int factorial(int n) {
+        if (n == 1) return 1;
+        return factorial(n - 1) * n;
+    }
+}
