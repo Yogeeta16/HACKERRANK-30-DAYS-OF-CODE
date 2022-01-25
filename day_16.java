9460f1dc778b39058f0ad9c7da8d9a0085063816
yogeeta b <90635845+Yogeeta16@users.noreply.github.com>
2022-01-25 13:08:46 +0530

    Create day_16.java

---
+++ day_16.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args)  {
+         Scanner in = new Scanner(System.in);
+        String S = in.next();
+        
+        try {
+            int i = Integer.parseInt(S);
+            System.out.println(i);
+        } catch (Exception e) {
+            System.out.println("Bad String");
+        }
+    }
+}
